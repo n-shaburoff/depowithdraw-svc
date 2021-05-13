49a37bd244241b2da1c8889e067e28dd82bfc53a
Nikita Shaburov <nikita.shaburov1@gmail.com>
2021-05-13 15:13:41 +0300

    Added hd wallet frame fix

--- data/bsc/deriver.go
+++ data/bsc/deriver.go
@@ -1,1 +1,53 @@
 package bsc
+
+import (
+	"encoding/hex"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pkg/errors"
+	"math"
+)
+
+type Deriver struct {
+	key *hdkeychain.ExtendedKey
+}
+
+func NewDeriver(src string) (*Deriver, error) {
+	key, err := hdkeychain.NewKeyFromString(src)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse key")
+	}
+	return &Deriver{key}, nil
+}
+
+func (d *Deriver) ChildPrivate(i uint32) (string, error) {
+	child, err := d.key.Derive(i)
+	if err != nil {
+		return "", err
+	}
+
+	priv, err := child.ECPrivKey()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get private key")
+	}
+
+	return hex.EncodeToString(priv.Serialize()), nil
+}
+
+func (d *Deriver) ChildAddress(i uint64) (string, error) {
+	if i >= math.MaxUint32 {
+		panic("child overflow")
+	}
+	child, err := d.key.Derive(uint32(i))
+	if err != nil {
+		return  "", err
+	}
+
+	public, err := child.ECPubKey()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get public key")
+	}
+	address := crypto.PubkeyToAddress(*public.ToECDSA())
+
+	return address.Hex(), nil
+}
--- data/bsc/wallet.go
+++ data/bsc/wallet.go
@@ -1,1 +1,64 @@
 package bsc
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pkg/errors"
+)
+
+type Wallet struct {
+	hd    bool
+	master  *Deriver
+	keys  map[common.Address]ecdsa.PrivateKey
+}
+
+func NewHDWallet(hdPrivate string, n uint64) (*Wallet, error) {
+	master, err := NewDeriver(hdPrivate)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to init key deriver")
+	}
+
+	wallet := &Wallet{
+		hd: true,
+		master: master,
+		keys: make(map[common.Address]ecdsa.PrivateKey),
+	}
+
+	if err := wallet.extend(n); err != nil {
+		return nil, errors.Wrap(err, "failed to extend master")
+	}
+
+	return wallet, nil
+}
+
+func (wallet *Wallet) Import(raw []byte) (common.Address, error) {
+	priv, err := crypto.ToECDSA(raw)
+	if err != nil {
+		return common.Address{}, errors.Wrap(err, "failed to convert private key")
+	}
+	address := crypto.PubkeyToAddress(priv.PublicKey)
+	wallet.keys[address] = *priv
+	return address, nil
+}
+
+func (wallet *Wallet) extend(i uint64) error {
+	for uint64(len(wallet.keys)) < i {
+		child, err := wallet.master.ChildPrivate(uint32(len(wallet.keys)))
+		if err != nil {
+			return errors.Wrap(err, "failed to extend child")
+		}
+
+		raw, err := hex.DecodeString(child)
+		if err != nil {
+			return errors.Wrap(err, "failed decode private key")
+		}
+
+		if _, err := wallet.Import(raw); err != nil {
+			return errors.Wrap(err, "failed to import key")
+		}
+	}
+
+	return nil
+}
